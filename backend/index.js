57c785cb1ee87b8773dc445816b751a781c93341
Heruuu226 <soloherugaming@gmail.com>
2026-02-09 14:34:31 +0700

    Update koneksi database ke TiDB Cloud

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,10 @@ const db = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 4000,
+    ssl: {
+        rejectUnauthorized: true // Tambahkan ini agar aman ke cloud
+    },
     waitForConnections: true,
     connectionLimit: 10
 });
